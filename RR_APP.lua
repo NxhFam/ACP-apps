9c0d606a1b79303a69d5d9b16b3dd5c8b0c7fbf8
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-05-11 20:34:00 -0400

    Update RR_APP.lua

--- RR_APP.lua
+++ RR_APP.lua
@@ -1,34 +1,19 @@
 local sim = ac.getSim()
 local windowWidth = sim.windowWidth
-local icon = "radar"
-local time = 0
+local color = rgbm.colors.blue
 
 
 function script.drawUI()
 	ui.transparentWindow('radar', vec2(0, 0), vec2(windowWidth, 400), function ()
 		local textSize = ui.measureDWriteText("GO CHANGE YOUR FUCKIN NAME\nIT MIGHT BE MISSING SOMETHING???" , 50)
-		ui.dwriteDrawText("GO CHANGE YOUR FUCKIN NAME" , 50, vec2((windowWidth-textSize.x)/2, 0), rgbm.colors.red)
+		ui.dwriteDrawText("GO CHANGE YOUR FUCKIN NAME" , 50, vec2((windowWidth-textSize.x)/2, 0), color)
 	end)
 end
 
 function script.update(dt)
-	time = time + dt
-	ui.registerOnlineExtra(icon, "settings", function ()
-		ui.itemPopup(ui.MouseButton.Right, function ()
-			if ui.selectable('Item 1') then 
-				ui.dwriteDrawText("Item 1" , 10, vec2(0, 0), rgbm.colors.red)
-			end
-			if ui.selectable('Item 2') then 
-				ui.dwriteDrawText("Item 2" , 10, vec2(0, 0), rgbm.colors.red)
-			end
-			ui.separator()
-			if ui.selectable('Item 3') then
-				ui.dwriteDrawText("Item 3" , 10, vec2(0, 0), rgbm.colors.red)
-			end
-		end)
-	end)
-	if time > 10 then
-		ac.sendChatMessage("FUCKK ME")
-		time = 0
-	end
+	ui.beginPopupContextItem("id: string")
+		if ui.button('Please Work') then
+			color = rgbm.colors.red
+		end
+	ui.endPopup()
 end
