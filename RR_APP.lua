163a5921c99a6d2e2f9e7d879d8b08b951085695
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-05-11 20:39:29 -0400

    Update RR_APP.lua

--- RR_APP.lua
+++ RR_APP.lua
@@ -7,10 +7,8 @@ function script.drawUI()
 	ui.transparentWindow('radar', vec2(0, 0), vec2(windowWidth, 400), function ()
 		local textSize = ui.measureDWriteText("GO CHANGE YOUR FUCKIN NAME\nIT MIGHT BE MISSING SOMETHING???" , 50)
 		ui.dwriteDrawText("GO CHANGE YOUR FUCKIN NAME" , 50, vec2((windowWidth-textSize.x)/2, 0), color)
-	end)
-	ui.beginPopupContextItem('mon cul')
 		if ui.button('Please Work') then
 			color = rgbm.colors.red
 		end
-	ui.endPopup()
+	end)
 end
