736fa3aee52e3844f3d65ed745a8600045d6608e
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-06-25 23:35:38 -0400

    Update ACP_Police.lua

--- ACP_Police.lua
+++ ACP_Police.lua
@@ -4,6 +4,7 @@ local windowWidth = sim.windowWidth
 local windowHeight = sim.windowHeight
 local menuOpen = false
 local settingsLoaded = true
+local valideCar = {"chargerpolice_acpursuit", "ks_mclaren_650_gt3"}
 
 local sharedDataSettings = ac.connect({
 	ac.StructItem.key('ACP_essential_settings'),
@@ -23,8 +24,7 @@ local sharedDataSettings = ac.connect({
 	msgOffsetX = ac.StructItem.int16(),
 	fontSizeMSG = ac.StructItem.int16(),
 	menuPos = ac.StructItem.vec2(),
-	unit = ac.StructItem.string(4),
-	unitMult = ac.StructItem.float(),
+	unit = ac.StructItem.string(4)
 }, true, ac.SharedNamespace.Shared)
 
 ui.setAsynchronousImagesLoading(true)
@@ -37,27 +37,35 @@ local hudMenu = assetsFolder .. "iconMenu.png"
 
 local msgChase = {
     {
+        lvl = 1,
         msg = {"This is the police! Please pull over to the side of the road!","You are requested to stop your `CAR` immediately, pull over now!","Attention driver, pull over and cooperate with the authorities.","Stop your `CAR` and comply with the police, this is a warning!","Stop the `CAR`! pull over to the side of the road, and follow our instructions."}
     },
     {
+        lvl = 2,
         msg = {"** Pull over now, failure to comply may result in consequences.","** We have reason to believe that you are evading the police in your `CAR`, pull over immediately.","** Stop your `CAR`, this is your last warning before we take action.","** You have been warned, failure to stop will result in the use of force.","** Pull over now or face the consequences, you have been warned."}
     },
     {
+        lvl = 3,
         msg = {"** This is your final warning, pull over and comply with the police!","** Stop your `CAR`, any attempt to evade the police will result in immediate action!","** You are endangering the public, pull over now and cooperate!","** Failure to comply with police orders will result in the use of force!","** Stop the `CAR` immediately, you are putting yourself and others in danger!",}
     },
     {
+        lvl = 4,
         msg = {"*** The use of force may be necessary if you do not comply, pull over now!!","*** You are putting the lives of others in danger, pull over and face the consequences!!","*** Pull over and surrender now, resistance will not be tolerated!!","*** This is the last warning, pull over and face the consequences of your actions!!","*** Stop the `CAR` immediately, you are putting yourself and others in danger!!",}
     },
     {
+        lvl = 5,
         msg = {"*** We are taking control of the situation, pull over and surrender now!","*** You have left us no choice, pull over or we will be forced to act!","*** Stop the `CAR` immediately, you are risking the lives of others!","*** This is your final warning, pull over or face the consequences!","*** Stop the vehicle and surrender now, the use of force is authorized!",}
     },
     {
+        lvl = 6,
         msg = {"**** The situation is escalating, pull over and surrender yourself to the authorities!","**** Your actions have consequences, pull over and face them now!","**** This is your last chance to comply, stop the `CAR` immediately!","**** We have authorization to use force, pull over and surrender!","**** You are putting yourself and others in danger, pull over now and cooperate!",}
     },
     {
+        lvl = 7,
         msg = {"**** You are risking the lives of innocent people, pull over and surrender now!","**** The use of force is imminent, pull over and surrender yourself to the police!","**** This is your final warning, pull over or face the full force of the law!","**** We will use any means necessary to stop your `CAR`, pull over now!","**** You have been warned, pull over and face the consequences of your actions!",}
     },
     {
+        lvl = 8,
         msg = {"***** This is your final warning, stop your `CAR` or we will use total force!","***** The situation has escalated, you must stop your `CAR` immediately or face the consequences!","***** We have authorization to use all necessary means to stop your `CAR`, stop now!","***** This is your last warning, stop your `CAR` or we will use all necessary force!","***** Stop your `CAR` immediately, or you will be met with total force!",}
     }
 }
@@ -69,8 +77,6 @@ local msgEngage = {
 local msgArrest = {
     msg = {"`NAME` driving a `CAR` has been arrested for Speeding","`NAME` driving a `CAR` has been arrested for Illegal Racing.","`NAME` driving a `CAR` has been arrested for Hit and Run.","`NAME` driving a `CAR` has been arrested for Car Theft.","`NAME` driving a `CAR` has been arrested for Evading Police.","`NAME` driving a `CAR` has been arrested for Public Disturbance."}
 }
-
-local valideCar = {"chargerpolice_acpursuit", "f40_acp2023"}
 
 local pursuit = {
 	suspect = nil,
@@ -87,12 +93,6 @@ local pursuit = {
 
 local arrestations = {}
 
-local textSize = {}
-
-local textPos = {}
-
-local iconPos = {}
-
 ---------------------------------------------------------------------------------------------- Settings ----------------------------------------------------------------------------------------------
 
 local acpPolice = ac.OnlineEvent({
@@ -100,22 +100,6 @@ local acpPolice = ac.OnlineEvent({
 	messageType = ac.StructItem.int16(),
 	yourIndex = ac.StructItem.int16(),
 }, function (sender, data) end)
-
-local function updatePos()
-	iconPos.arrest1 = vec2(imageSize.x - imageSize.x/1.7, imageSize.y/1.9)
-	iconPos.arrest2 = vec2(imageSize.x/3.6, imageSize.y/2.6)
-	iconPos.menu1 = vec2(imageSize.x/1.75, imageSize.y/1.9)
-	iconPos.menu2 = vec2(imageSize.x - imageSize.x/1.75, imageSize.y/2.6)
-
-	textSize.size = vec2(imageSize.x*3/5, SETTINGS.statsFont/2)
-	textSize.box = vec2(imageSize.x*3/5, SETTINGS.statsFont/2*1.2)
-	textSize.window1 = vec2(SETTINGS.statsOffsetX+imageSize.x/10, SETTINGS.statsOffsetY+imageSize.y/5)
-	textSize.window2 = vec2(imageSize.x*3/5, imageSize.y/4)
-
-	textPos.box1 = vec2(0, 0)
-	textPos.box2 = vec2(textSize.size.x, textSize.size.y*1.6)
-	textPos.addBox = vec2(0, textSize.size.y*1.6)
-end
 
 local showPreviewMsg = false
 COLORSMSGBG = rgbm(0.5,0.5,0.5,0.5)
@@ -147,7 +131,6 @@ local function initSettings()
 	if SETTINGS.unit ~= "Kmh" then SETTINGS.unitMult = 0.621371 end
 	SETTINGS.statsFont = SETTINGS.statsSize * windowHeight/1440
 	imageSize = vec2(windowHeight/80 * SETTINGS.statsSize, windowHeight/80 * SETTINGS.statsSize)
-	updatePos()
 end
 
 local function previewMSG()
@@ -179,6 +162,7 @@ local function uiTab()
 	ui.newLine()
 end
 
+
 local function settings()
 	imageSize = vec2(windowHeight/80 * SETTINGS.statsSize, windowHeight/80 * SETTINGS.statsSize)
 	if ui.checkbox('Show HUD', SETTINGS.showStats) then SETTINGS.showStats = not SETTINGS.showStats end
@@ -192,7 +176,6 @@ local function settings()
     ui.colorPicker('Theme Color', colorHud, ui.ColorPickerFlags.AlphaBar)
     ui.newLine()
     uiTab()
-	updatePos()
     return 2
 end
 
@@ -206,7 +189,7 @@ local function formatMessage(message)
 		msgToSend = string.gsub(msgToSend,"`SPEED`", "No Speed")
 		return msgToSend
 	end
-	msgToSend = string.gsub(msgToSend,"`CAR`", string.gsub(string.gsub(ac.getCarName(pursuit.suspect.index), "%W", " "), "  ", ""))
+	msgToSend = string.gsub(msgToSend,"`CAR`", string.gsub(string.gsub(ac.getCarName(0), "%W", " "), "  ", ""))
 	msgToSend = string.gsub(msgToSend,"`NAME`", "@" .. ac.getDriverName(pursuit.suspect.index))
 	msgToSend = string.gsub(msgToSend,"`SPEED`", string.format("%d ", ac.getCarSpeedKmh(pursuit.suspect.index) * SETTINGS.unitMult) .. SETTINGS.unit)
 	return msgToSend
@@ -225,21 +208,25 @@ local function drawImage()
 	iconsColorOn[1] = rgbm(0.99,0.99,0.99,1)
 	iconsColorOn[2] = rgbm(0.99,0.99,0.99,1)
 	local uiStats = ac.getUI()
+	local arrestPos1 = vec2(imageSize.x - imageSize.x/1.7, imageSize.y/1.9)
+	local arrestPos2 = vec2(imageSize.x/3.6, imageSize.y/2.6)
+	local menuPos1 = vec2(imageSize.x/1.75, imageSize.y/1.9)
+	local menuPos2 = vec2(imageSize.x - imageSize.x/1.75, imageSize.y/2.6)
 
 	ui.drawImage(hudCenter, vec2(0,0), imageSize)
-	if ui.rectHovered(iconPos.arrest2, iconPos.arrest1) then
+	if ui.rectHovered(arrestPos2, arrestPos1) then
 		iconsColorOn[1] = SETTINGS.colorHud
 		if pursuit.suspect and car.speedKmh < 20 and uiStats.isMouseLeftKeyClicked then
 			pursuit.hasArrested = true
 		end
-	elseif ui.rectHovered(iconPos.menu2, iconPos.menu1) then
+	elseif ui.rectHovered(menuPos2, menuPos1) then
 		iconsColorOn[2] = SETTINGS.colorHud
 		if uiStats.isMouseLeftKeyClicked then
 			if menuOpen then menuOpen = false else menuOpen = true end
 		end
 	end
 	ui.image(hudBase, imageSize, SETTINGS.colorHud)
-	--ui.drawImage(hudArrest, vec2(0,0), imageSize, iconsColorOn[1])
+	ui.drawImage(hudArrest, vec2(0,0), imageSize, iconsColorOn[1])
 	ui.drawImage(hudMenu, vec2(0,0), imageSize, iconsColorOn[2])
 end
 
@@ -253,40 +240,45 @@ local function playerSelected(player)
 		pursuit.nextMessage = 20
 		pursuit.level = 1
 		ac.setExtraSwitch(0, true)
-		ac.log(formatMessage(msgEngage.msg[math.random(#msgEngage.msg)]))
+		ac.sendChatMessage(formatMessage(msg.engage.msg[math.random(msg.engage.nb)]))
 	end
 end
 
 local function drawText()
-	ui.pushDWriteFont("Orbitron;Weight=Black")
+	local text
+	local textSize = ui.measureDWriteText("MMMMMMMMMMMMMMMMMMMM" .. string.format(" %d ", 000) .. SETTINGS.unit, SETTINGS.statsFont)
+	local firstBox = vec2(123,123)
 	local uiStats = ac.getUI()
-	local textColor = rgbm(1,1,1,1)
-	textPos.box1 = vec2(0, 0)
-	for i = 1, #playersInRange do		
-		textColor = rgbm(1,1,1,1)
-		--ui.drawRect(textPos.box1, textPos.box1 + textPos.box2, rgbm(0,0,1,1))
-		if ui.rectHovered(textPos.box1, textPos.box1 + textPos.box2) then
-			textColor = rgbm(0,1,1,1)
+
+	for i = 1, #playersInRange do
+		text = string.sub(ac.getDriverName(playersInRange[i].index), 1, 20) .. string.format(" %d ", playersInRange[i].speedKmh * SETTINGS.unitMult) .. SETTINGS.unit
+		if ui.rectHovered(firstBox, firstBox + textSize) then
 			if uiStats.isMouseLeftKeyClicked then
-				playerSelected(playersInRange[i].player)
+				ui.dwriteText(text, SETTINGS.statsFont, SETTINGS.colorHud)
+				playerSelected(playersInRange[i])
 			end
-		elseif pursuit.suspect == playersInRange[i].player then
-			textColor = SETTINGS.colorHud
+		elseif pursuit.suspect == playersInRange[i] then
+			ui.dwriteDrawText(text, SETTINGS.statsFont, firstBox, SETTINGS.colorHud)
+		else
+			ui.dwriteDrawText(text, SETTINGS.statsFont, firstBox, rgbm(1,1,1,1))
 		end
-		textPos.box1 = textPos.box1 + textPos.addBox
-		ui.dwriteTextAligned(playersInRange[i].text, SETTINGS.statsFont/2, ui.Alignment.Center, ui.Alignment.Center, textSize.box, false, textColor)
+		if i % 2 == 0 then
+			firstBox = firstBox + vec2(textSize.x, 0)
+		else
+			firstBox = firstBox + vec2(-textSize.x, textSize.y)
+		end
 	end
-	ui.popDWriteFont()
 end
 
 local function radarUI()
-	ui.transparentWindow('radar', vec2(SETTINGS.statsOffsetX, SETTINGS.statsOffsetY), imageSize, true, function ()
+	ui.transparentWindow('RadarHud', vec2(SETTINGS.statsOffsetX, SETTINGS.statsOffsetY), imageSize, true, function ()
 		drawImage()
-	end)
-	ui.toolWindow('radarText', textSize.window1, textSize.window2, true, function ()
-		ui.childWindow('childradar', true, function ()
+		ui.childWindow('PlayerList', imageSize/2, false, function ()
 			drawText()
 		end)
+		if pursuit.suspect then
+			ui.dwriteTextWrapped("Driver : " .. ac.getDriverName(pursuit.suspect.index) .. "\nCar : " .. ac.getCarName(pursuit.suspect.index) .. string.format(" %d ", ac.getCarSpeedKmh(pursuit.suspect.index) * SETTINGS.unitMult) .. SETTINGS.unit .. "\nPursuit : " .. string.format("%.1f", os.clock() - pursuit.timeInPursuit) .. "s LVL :    " .. pursuit.level, SETTINGS.statsFont * 2, SETTINGS.colorHud)
+		end
 	end)
 end
 
@@ -300,9 +292,7 @@ local function radarUpdate()
 		if player.isConnected and (not player.isHidingLabels) then
 			if player.index ~= car.index then
 				if player.position.x > car.position.x - radarRange and player.position.z > car.position.z - radarRange and player.position.x < car.position.x + radarRange and player.position.z < car.position.z + radarRange then
-					playersInRange[j] = {}
-					playersInRange[j].player = player
-					playersInRange[j].text = ac.getDriverName(player.index) .. string.format(" %d ", player.speedKmh * 1) .. SETTINGS.unit
+					playersInRange[j] = player
 					j = j + 1
 					if j == 9 then break end
 				end
@@ -342,7 +332,7 @@ local function inRange()
 	else
 		if pursuit.suspect.rpm > 400 and pursuit.suspect.speedKmh > 20 then
 			local msgToSend = formatMessage("Suspect have been lost, Vehicle Description:`CAR` driven by `NAME`")
-			ac.log(msgToSend)
+			ac.sendChatMessage(msgToSend)
 		end
 		lostSuspect()
 	end
@@ -351,13 +341,14 @@ end
 local function sendChaseMsg()
 	if pursuit.enable then
 		if os.clock() - pursuit.timeInPursuit > pursuit.nextMessage then
-			local msgToSend = formatMessage(msgChase[pursuit.level].msg[math.random(#msgChase[pursuit.level].msg)])
+			local i = math.random(msg.stop[pursuit.level].nb)
+			local msgToSend = formatMessage(msg.stop[pursuit.level].msg[i])
 			chaseLVL.message = string.format("Level %d⭐", pursuit.level)
 			chaseLVL.messageTimer = SETTINGS.timeMsg
-			if pursuit.level < 5 then
+			if pursuit.level < 5 and pursuit.connected then
 				acpPolice{message = msgToSend, messageType = 1, yourIndex = ac.getCar(pursuit.suspect.index).sessionID}
 			else
-				ac.log(msgToSend)
+				ac.sendChatMessage(msgToSend)
 			end
 			pursuit.nextMessage = pursuit.nextMessage + 20
 			if pursuit.level < 8 then
@@ -386,23 +377,21 @@ end
 
 local function arrestSuspect()
 	if pursuit.hasArrested and pursuit.suspect then
-		local msgToSend = formatMessage(msgArrest.msg[math.random(#msgArrest.msg)])
+		local msgToSend = formatMessage(msg.arrest.msg[b])
 		table.insert(arrestations, msgToSend .. os.date("\nDate of the Arrestation: %c"))
-		ac.log("You are under arrest!\n" .. msgToSend .. "\nPlease Get Back Pit, GG!")
+		ac.sendChatMessage("You are under arrest!\n" .. msgToSend .. "\nPlease Get Back Pit, GG!")
 		pursuit.id = pursuit.suspect.sessionID
 		pursuit.suspect = nil
 		ac.setExtraSwitch(0, false)
 		pursuit.timerArrest = 1
 	end
-	if pursuit.hasArrested and not pursuit.suspect then
-		if pursuit.timerArrest > 0 then
-			pursuit.timerArrest = pursuit.timerArrest - ui.deltaTime()
-		else
-			acpPolice{message = "Arrest", messageType = 2, yourIndex = pursuit.id}
-			pursuit.timerArrest = 0
-			pursuit.suspect = nil
-			pursuit.id = -1
-		end
+	if pursuit.timerArrest > 0 then
+		pursuit.timerArrest = pursuit.timerArrest - ui.deltaTime()
+	else
+		acpPolice{message = "Arrest", messageType = 2, yourIndex = pursuit.id}
+		pursuit.timerArrest = 0
+		pursuit.suspect = nil
+		pursuit.id = -1
 	end
 end
 
@@ -483,17 +472,21 @@ function script.drawUI()
 end
 
 function script.update(dt)
-	--if ac.getCarID(0) ~= valideCar[1] and ac.getCarID(0) ~= valideCar[2] then return end
 	if not initialized then
+		if ac.getCarID(0) ~= valideCar[1] and ac.getCarID(0) ~= valideCar[2] then return end
 		initialized = true
         initSettings()
+		ac.log("notinit")
 	else
 		if settingsLoaded then
 			radarUpdate()
 			chaseUpdate()
+			ac.log("init")
 			sharedDataSettings = SETTINGS
 		end
 	end
 end
 
-ui.registerOnlineExtra(ui.Icons.Menu, 'Menu', nil, menu, nil, ui.OnlineExtraFlags.Tool)
+-- if ac.getCarID(0) == valideCar[1] or ac.getCarID(0) == valideCar[2] then
+-- 	ui.registerOnlineExtra(ui.Icons.Menu, 'Menu', nil, menu, nil, ui.OnlineExtraFlags.Tool)
+-- end
