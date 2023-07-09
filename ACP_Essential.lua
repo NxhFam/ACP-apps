8ab7b5344944734878989f25f93a558a238d7f92
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-07-09 12:22:00 -0400

    Update ACP_Essential.lua

--- ACP_Essential.lua
+++ ACP_Essential.lua
@@ -551,8 +551,8 @@ local function getFirebase()
 			else
 				local jString = response.body
 				playerData = json.parse(jString)
-				if playerData.WR == nil then table.insert(playerData, {WR = 0}) end
-				if playerData.Arrests == nil then table.insert(playerData, {Arrests = 0}) end
+				if playerData.WR == nil then playerData.WR = 0 end
+				if playerData.Arrests == nil then playerData.Arrests = 0 end
 			end
 		end
 	end)
