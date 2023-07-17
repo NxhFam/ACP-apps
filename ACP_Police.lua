618c810c49c7f6468bd9f84d5c6091cf8a625d41
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-07-16 23:03:36 -0400

    Update ACP_Police.lua

--- ACP_Police.lua
+++ ACP_Police.lua
@@ -920,12 +920,6 @@ local function chaseUpdate()
 	else pursuit.startedTime = 0 end
 	if pursuit.suspect then
 		ac.log("Chase Update")
-		ac.onCarJumped(pursuit.suspect.index, function (carid)
-			pursuit.hasArrested = true
-			pursuit.hasJumped = true
-			ac.log("Suspect has jumped")
-			arrestSuspect()
-		end)
 		sendChatToSuspect()
 		inRange()
 	end
@@ -1042,6 +1036,16 @@ function script.update(dt)
 		chaseUpdate()
 	end
 end
+
+ac.onCarJumped(-1, function (carid)
+	if pursuit.suspect and carid == pursuit.suspect.index then
+		pursuit.hasArrested = true
+		pursuit.hasJumped = true
+		ac.log("Suspect has jumped")
+		arrestSuspect()
+	end
+	ac.log("Car jumped")
+end)
 
 if carID == valideCar[1] or carID == valideCar[2] and cspVersion >= cspMinVersion then
 	ui.registerOnlineExtra("Menu", "Menu", nil, settingsWindow, nil, ui.OnlineExtraFlags.Tool, 'ui.WindowFlags.AlwaysAutoResize')
