159644428df69fb86bc5a34db5897bbe45bfd69c
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2023-08-21 12:34:28 -0400

    Update ACP_Essential.lua

--- ACP_Essential.lua
+++ ACP_Essential.lua
@@ -21,7 +21,6 @@ local valideCar = {"chargerpolice_acpursuit", "crown_police"}
 local fontMultiplier = windowHeight/1440
 local carID = ac.getCarID(0)
 local wheels = car.wheels
-local rx7Valid = ac.INIConfig.carData(0, "brakes.ini"):get("DATA", "MAX_TORQUE", 0) == 4100 and ac.getCarID(0) == "rx7_2_acpursuit"
 
 if carID == valideCar[1] or carID == valideCar[2] or cspVersion < cspMinVersion then return end
 
@@ -323,21 +322,13 @@ local sectors  = {
     },
 	{
 		name = 'Velocity Vendetta',
-		pointsData = {{vec3(-155,10,-320.6), vec3(-146.5,10,-327.4)},
-                    {vec3(118.1,41.8,397.7), vec3(129.1,42.2,409.9)},
-                    {vec3(-270,23.6,-20.6), vec3(-270.2,23.4,-31.7)},
-                    {vec3(-274.7,35.6,403.8), vec3(-265,35.7,402.9)},
-                    {vec3(-205.7,23.7,-41.2), vec3(-217.6,23.5,-44.8)},
-                    {vec3(-173.3,19.7,-102.5), vec3(-177.1,19.2,-114.2)},
-                    {vec3(139.9,39.4,308.3), vec3(129.2,39.4,314.7)}},
-		linesData = {vec4(-155, -320.6, -146.5, -327.4),
-                    vec4(129.1, 409.9, 118.1, 397.7),
-                    vec4(-270, -20.6, -270.2, -31.7),
-                    vec4(-274.7, 403.8, -265, 402.9),
-                    vec4(-217.6, -44.8, -205.7, -41.2),
-                    vec4(-177.1, -114.2, -173.3, -102.5),
-                    vec4(139.9, 308.3, 129.2, 314.7)},
-		length = 6.6,
+		pointsData = {{vec3(-3951.9,-184.7,10007.2), vec3(-3944.9,-184.7,10004.4)},
+					{vec3(-5774.6,-349.1,10183.9), vec3(-5776.7,-349.2,10173.8)},
+					{vec3(-3977.2,-147.5,9537.4), vec3(-3969.2,-147.6,9540.2)}},
+		linesData = {vec4(-3944.9,10004.4,-3951.9,10007.2),
+					vec4(-5774.6,10183.9,-5776.7,10173.8),
+					vec4(-3977.2,9537.4,-3969.2,9540.2)},
+		length = 4,
 	},
 	{
 		name = 'JDM LEGENDS',
@@ -1179,10 +1170,6 @@ local function sectorUI()
 		sectorInfo.sectorIndex = 1
 		resetSectors()
 	end
-    if sectorInfo.sectorIndex == 4 and not rx7Valid then
-        ui.newLine()
-        ui.dwriteTextWrapped("You need to have the Rental RX-7 to do this sector", 15, rgbm.colors.white)
-    end
 	if sectorInfo.sectorIndex == 3 then doubleTrouble() end
 	if duo.request then
 		ui.newLine()
@@ -1259,7 +1246,7 @@ local function sectorUpdate()
 					updatefirebase()
 				else
 					if sectors[sectorInfo.sectorIndex].name == "H1" then updateSectorData('H1', sectorInfo.time)
-					elseif sectors[sectorInfo.sectorIndex].name == "Velocity Vendetta" and rx7Valid then updateSectorData('VV', sectorInfo.time)
+					elseif sectors[sectorInfo.sectorIndex].name == "Velocity Vendetta" then updateSectorData('VV', sectorInfo.time)
 					elseif sectors[sectorInfo.sectorIndex].name == "JDM LEGENDS" then updateSectorData('JDM', sectorInfo.time) end
 					ac.sendChatMessage(" has finished " .. sectors[sectorInfo.sectorIndex].name .. " in " .. sectorInfo.timerText .. "!")
 				end
@@ -2280,6 +2267,9 @@ ac.onChatMessage(function (message, senderCarIndex, senderSessionID)
 end)
 
 function script.draw3D()
+	render.setBlendMode(render.BlendMode.AlphaBlend)
+    render.setCullMode(render.CullMode.None)
+	render.setDepthMode(render.DepthMode.Normal)
 	if initialized and settings.current == 4 then
 		local lineToRender = sector.pointsData[sectorInfo.checkpoints]
 		if sectorInfo.drawLine then render.debugLine(lineToRender[1], lineToRender[2], rgbm(0,100,0,1)) end
@@ -2289,4 +2279,3 @@ end
 if carID ~= valideCar[1] and carID ~= valideCar[2] and cspVersion >= cspMinVersion then
 	ui.registerOnlineExtra(ui.Icons.Menu, "Menu", nil, menu, nil, ui.OnlineExtraFlags.Tool, 'ui.WindowFlags.AlwaysAutoResize')
 end
-
