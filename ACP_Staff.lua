7543a5477401df9a85ab189f02c8aef6594a104a
Émile Le Sage <83116125+ele-sage@users.noreply.github.com>
2024-03-15 21:39:42 -0400

    Update ACP_Staff.lua

--- ACP_Staff.lua
+++ ACP_Staff.lua
@@ -1,1 +1,11 @@
-return
+local initialized = false
+
+function script.update(dt)
+	if not initialized then
+	  ac.log(sim.directUDPMessagingAvailable)
+    ac.log("Staff script")
+    initialized = true
+  else
+    return
+	end
+end
